3d502e37139c3eced338461afcb295ee75b02a4e
lesds <lesds@zentera.net>
2018-10-21 10:11:23 -0700

    Minor Updates

--- Zapy/__init__.py
+++ Zapy/__init__.py
@@ -1,3 +1,4 @@
+name = "Zapy"
 from keyChain import keyChain
 from AppProfile import AppProfile
 from CloudDomain import CloudDomain
@@ -6,8 +7,10 @@ from CoipLan import CoipLan
 from CoipWanTypeOne import CoipWanTypeOne
 from CoipWanTypeTwo import CoipWanTypeTwo
 from CoipWanTypeThree import CoipWanTypeThree
+from ComputingFlow import ComputingFlow
 from EndServer import EndServer
 from FabricServerPool import FabricServerPool
+from IpComponent import IpComponent
 from InlineDeviceGroup import InlineDeviceGroup
 from ServerGroup import ServerGroup
 from ZaUser import ZaUser
